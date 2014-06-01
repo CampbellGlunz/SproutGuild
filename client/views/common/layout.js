8f7ca93316bf4b884dba18e4f655fab86ce3d49f
Zandrr <alexander.campbell@colorado.edu>
2014-06-01 16:52:56 -0600

    fixed login dropdown when logged in

--- client/views/common/layout.js
+++ client/views/common/layout.js
@@ -40,9 +40,10 @@ Template.layout.events({
 
   'click .outer-wrapper':function(e){
       var container = $("#login-dropdown-list");
+      var container2 = $("#login-name-link");
 
-      if (!container.is(e.target) // if the target of the click isn't the container...
-          && container.has(e.target).length === 0) // ... nor a descendant of the container
+      if (!container.is(e.target) && !container2.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0 && container2.has(e.target).length === 0)// ... nor a descendant of the container
       {
         Accounts._loginButtonsSession.closeDropdown();
       }
