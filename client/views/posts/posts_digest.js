8b1b4b36de91e210a84ac068e02149b3dfb4ac4a
Danny Glunz <dannyglunz@gmail.com>
2014-06-13 19:21:46 -0600

    upped base limit to 100 for now, need to define better

--- client/views/posts/posts_digest.js
+++ client/views/posts/posts_digest.js
@@ -2,36 +2,36 @@ Template.posts_digest.helpers({
   hasPosts: function(){
     if(this.dayposts) // XXX
       return !!this.dayposts.count();  
-  },
-  currentDate: function(){
-    var currentDate=moment(Session.get('currentDate'));
-    var today=moment(new Date());
-    var diff=today.diff(currentDate, 'days');
-    if(diff === 1)
-      return i18n.t("Today");
-    if(diff === 2)
-      return i18n.t("Yesterday");
-    return currentDate.format("dddd, MMMM Do YYYY");
-  },
-  previousDateURL: function(){
-    var currentDate=moment(Session.get('currentDate'));
-    var newDate=currentDate.subtract('days', 1);
-    return getDigestURL(newDate);
-  },
-  showPreviousDate: function(){
-    // TODO
-    return true;
-  },
-  nextDateURL: function(){
-    var currentDate=moment(Session.get('currentDate'));
-    var newDate=currentDate.add('days', 1);
-    return getDigestURL(newDate);
-  },
-  showNextDate: function(){
-    var currentDate=moment(Session.get('currentDate')).startOf('day');
-    var today=moment(new Date()).startOf('day');
-    return isAdmin(Meteor.user()) || (today.diff(currentDate, 'days') > 0)
   }
+  // currentDate: function(){
+  //   var currentDate=moment(Session.get('currentDate'));
+  //   var today=moment(new Date());
+  //   var diff=today.diff(currentDate, 'days');
+  //   if(diff === 1)
+  //     return i18n.t("Today");
+  //   if(diff === 2)
+  //     return i18n.t("Yesterday");
+  //   return currentDate.format("dddd, MMMM Do YYYY");
+  // },
+  // previousDateURL: function(){
+  //   var currentDate=moment(Session.get('currentDate'));
+  //   var newDate=currentDate.subtract('days', 1);
+  //   return getDigestURL(newDate);
+  // },
+  // showPreviousDate: function(){
+  //   // TODO
+  //   return true;
+  // },
+  // nextDateURL: function(){
+  //   var currentDate=moment(Session.get('currentDate'));
+  //   var newDate=currentDate.add('days', 1);
+  //   return getDigestURL(newDate);
+  // },
+  // showNextDate: function(){
+  //   var currentDate=moment(Session.get('currentDate')).startOf('day');
+  //   var today=moment(new Date()).startOf('day');
+  //   return isAdmin(Meteor.user()) || (today.diff(currentDate, 'days') > 0)
+  // }
 });
 
 Template.posts_digest.created = function(){
