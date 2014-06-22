6bcbc1a2b1bb26bff8a27ad0ce2e812f97930f59
Danny Glunz <dannyglunz@gmail.com>
2014-06-22 14:11:54 -0600

    added company name to headline

--- server/rss.js
+++ server/rss.js
@@ -1,7 +1,7 @@
 serveRSS = function() {
   var feed = new RSS({
     title: getSetting('title', 'Sprout Guild'),
-    description: getSetting('tagline'),
+    description: 'Daily Discovery of the Hippest New Natural Foods, Products & Brands',
     feed_url: Meteor.absoluteUrl()+'feed.xml',
     site_url: Meteor.absoluteUrl(),
     image_url: Meteor.absoluteUrl()+'img/favicon.png',
@@ -9,9 +9,8 @@ serveRSS = function() {
   
   Posts.find({status: STATUS_APPROVED}, {sort: {submitted: -1}, limit: 20}).forEach(function(post) {
     feed.item({
-     company: post.companyName,
-     title: post.headline,
-     description: post.body+'</br></br> <a href="'+getPostUrl(post._id)+'">Comments</a>',
+     title: post.headline + " - " + post.companyName,
+     description: post.body+'</br></br> <a href="'+getPostUrl(post._id)+'">Comments</a>',         
      author: post.author,
      date: post.submitted,
      url: (post.url ? getOutgoingUrl(post.url) : getPostUrl(post._id)),
