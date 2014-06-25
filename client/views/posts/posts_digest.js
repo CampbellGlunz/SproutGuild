969dfd01372f68a2c578bbc635857ad99762c9ff
Danny Glunz <dannyglunz@gmail.com>
2014-06-25 00:20:19 -0600

    cleaned up code/implemented working pagination for daily view

--- client/views/posts/posts_digest.js
+++ client/views/posts/posts_digest.js
@@ -5,16 +5,12 @@ Template.posts_digest.helpers({
   },
 });
 
-Template.posts_digest.created = function(){
-  $(document).unbind('keyup'); //remove any potential existing bindings to avoid duplicates
-  var currentDate=moment(Session.get('currentDate')).startOf('day');
-  var today=moment(new Date()).startOf('daysy');
-  $(document).bind('keyup', 'left', function(){
-    Router.go($('.prev-link').attr('href'));
-  });
-  $(document).bind('keyup', 'right', function(){
-    if(isAdmin(Meteor.user()) || today.diff(currentDate, 'days') > 0)
-      Router.go($('.next-link').attr('href'));      
-  });  
-};
+Template.posts_digest.events ({
+  'click .more-link': function(e){
+   $('html,body').animate({
+    scrollTop: $(window).scrollTop() 
+      }, 500);
+}
+
+});
 
